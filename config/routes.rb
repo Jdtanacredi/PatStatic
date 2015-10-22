1892d37b4d98e01eae7d23a0315db34e2c5ccf88
Justin Tanacredi <jtanacredi@gmail.com>
2015-10-22 09:25:19 -0400

    Add launch insta page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # get 'products/index'
-
   # get 'products/show'
   get '*path' => redirect('/')
 
   root :to => 'mcsubscribe#index'
+  # root :to => 'mcsubscribe#launch'
   post 'mcsubscribe/subscribe', to: 'mcsubscribe#subscribe'
-  # get 'mcsubscribe/launch'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
